253e2d0db2f17ba3fae607e31e8b7bed5c05cdc5
james-tozer <jamestozer@economist.com>
2020-06-25 12:51:54 +0100

    Added compiled files for excess deaths

--- scripts/excess_deaths_script.R
+++ scripts/excess_deaths_script.R
@@ -64,121 +64,166 @@ get_excess_deaths <- function(df,frequency="weekly",calculate=TRUE){
   # Calculate excess deaths
   excess_deaths <- expected_deaths %>%
     mutate(excess_deaths = total_deaths - expected_deaths,
-           non_covid_deaths = total_deaths - covid_deaths)
+           non_covid_deaths = total_deaths - covid_deaths,
+           region_code = as.character(region_code))
   
 }
 
 # Export Austria
-write.csv(get_excess_deaths(austria_weekly_deaths,calculate=FALSE),
-       "output-data/excess-deaths/austria_excess_deaths.csv",
+austria_excess_deaths <- get_excess_deaths(austria_weekly_deaths,calculate=FALSE)
+write.csv(austria_excess_deaths,"output-data/excess-deaths/austria_excess_deaths.csv",
        fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Belgium
-write.csv(get_excess_deaths(belgium_weekly_deaths,calculate=FALSE),
-          "output-data/excess-deaths/belgium_excess_deaths.csv",
+belgium_excess_deaths <- get_excess_deaths(belgium_weekly_deaths,calculate=FALSE)
+write.csv(belgium_excess_deaths,"output-data/excess-deaths/belgium_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Brazil
-write.csv(get_excess_deaths(brazil_monthly_deaths,frequency="monthly"),
-          "output-data/excess-deaths/brazil_excess_deaths.csv",
+brazil_excess_deaths <- get_excess_deaths(brazil_monthly_deaths,frequency="monthly")
+write.csv(brazil_excess_deaths,"output-data/excess-deaths/brazil_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Britain
-write.csv(get_excess_deaths(britain_weekly_deaths),
-          "output-data/excess-deaths/britain_excess_deaths.csv",
+britain_excess_deaths <- get_excess_deaths(britain_weekly_deaths)
+write.csv(britain_excess_deaths,"output-data/excess-deaths/britain_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Chile
-write.csv(get_excess_deaths(chile_weekly_deaths),
-          "output-data/excess-deaths/chile_excess_deaths.csv",
+chile_excess_deaths <- get_excess_deaths(chile_weekly_deaths)
+write.csv(chile_excess_deaths,"output-data/excess-deaths/chile_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Denmark
-write.csv(get_excess_deaths(denmark_weekly_deaths),
-          "output-data/excess-deaths/denmark_excess_deaths.csv",
+denmark_excess_deaths <- get_excess_deaths(denmark_weekly_deaths)
+write.csv(denmark_excess_deaths,"output-data/excess-deaths/denmark_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Ecuador
-write.csv(get_excess_deaths(ecuador_monthly_deaths,frequency="monthly"),
-          "output-data/excess-deaths/ecuador_excess_deaths.csv",
+ecuador_excess_deaths <- get_excess_deaths(ecuador_monthly_deaths,frequency="monthly")
+write.csv(ecuador_excess_deaths,"output-data/excess-deaths/ecuador_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export France
-write.csv(get_excess_deaths(france_weekly_deaths),
-          "output-data/excess-deaths/france_excess_deaths.csv",
+france_excess_deaths <- get_excess_deaths(france_weekly_deaths)
+write.csv(france_excess_deaths,"output-data/excess-deaths/france_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Germany
-write.csv(get_excess_deaths(germany_weekly_deaths),
-          "output-data/excess-deaths/germany_excess_deaths.csv",
+germany_excess_deaths <- get_excess_deaths(germany_weekly_deaths)
+write.csv(germany_excess_deaths,"output-data/excess-deaths/germany_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Indonesia
-write.csv(get_excess_deaths(indonesia_monthly_deaths,frequency="monthly"),
-          "output-data/excess-deaths/indonesia_excess_deaths.csv",
+indonesia_excess_deaths <- get_excess_deaths(indonesia_monthly_deaths,frequency="monthly")
+write.csv(indonesia_excess_deaths,"output-data/excess-deaths/indonesia_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Italy
-write.csv(get_excess_deaths(italy_weekly_deaths),
-          "output-data/excess-deaths/italy_excess_deaths.csv",
+italy_excess_deaths <- get_excess_deaths(italy_weekly_deaths)
+write.csv(italy_excess_deaths,"output-data/excess-deaths/italy_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Mexico
-write.csv(get_excess_deaths(mexico_weekly_deaths),
-          "output-data/excess-deaths/mexico_excess_deaths.csv",
+mexico_excess_deaths <- get_excess_deaths(mexico_weekly_deaths)
+write.csv(mexico_excess_deaths,"output-data/excess-deaths/mexico_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export the Netherlands
-write.csv(get_excess_deaths(netherlands_weekly_deaths),
-          "output-data/excess-deaths/netherlands_excess_deaths.csv",
+netherlands_excess_deaths <- get_excess_deaths(netherlands_weekly_deaths)
+write.csv(netherlands_excess_deaths,"output-data/excess-deaths/netherlands_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Norway
-write.csv(get_excess_deaths(norway_weekly_deaths),
-          "output-data/excess-deaths/norway_excess_deaths.csv",
+norway_excess_deaths <- get_excess_deaths(norway_weekly_deaths)
+write.csv(norway_excess_deaths,"output-data/excess-deaths/norway_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Peru
-write.csv(get_excess_deaths(peru_monthly_deaths,frequency="monthly"),
-          "output-data/excess-deaths/peru_excess_deaths.csv",
+peru_excess_deaths <- get_excess_deaths(peru_monthly_deaths,frequency="monthly")
+write.csv(peru_excess_deaths,"output-data/excess-deaths/peru_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Portugal
-write.csv(get_excess_deaths(portugal_weekly_deaths),
-          "output-data/excess-deaths/portugal_excess_deaths.csv",
+portugal_excess_deaths <- get_excess_deaths(portugal_weekly_deaths)
+write.csv(portugal_excess_deaths,"output-data/excess-deaths/portugal_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Russia
-write.csv(get_excess_deaths(russia_monthly_deaths,frequency="monthly"),
-          "output-data/excess-deaths/russia_excess_deaths.csv",
+russia_excess_deaths <- get_excess_deaths(russia_monthly_deaths,frequency="monthly")
+write.csv(russia_excess_deaths,"output-data/excess-deaths/russia_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export South Africa
-write.csv(get_excess_deaths(south_africa_weekly_deaths,calculate=FALSE),
-          "output-data/excess-deaths/south_africa_excess_deaths.csv",
+south_africa_excess_deaths <- get_excess_deaths(south_africa_weekly_deaths,calculate=FALSE)
+write.csv(south_africa_excess_deaths,"output-data/excess-deaths/south_africa_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Spain
-write.csv(get_excess_deaths(spain_weekly_deaths,calculate=FALSE),
-          "output-data/excess-deaths/spain_excess_deaths.csv",
+spain_excess_deaths <- get_excess_deaths(spain_weekly_deaths,calculate=FALSE)
+write.csv(spain_excess_deaths,"output-data/excess-deaths/spain_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Sweden
-write.csv(get_excess_deaths(sweden_weekly_deaths),
-          "output-data/excess-deaths/sweden_excess_deaths.csv",
+sweden_excess_deaths <- get_excess_deaths(sweden_weekly_deaths)
+write.csv(sweden_excess_deaths,"output-data/excess-deaths/sweden_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Switzerland
-write.csv(get_excess_deaths(switzerland_weekly_deaths),
-          "output-data/excess-deaths/switzerland_excess_deaths.csv",
+switzerland_excess_deaths <- get_excess_deaths(switzerland_weekly_deaths)
+write.csv(switzerland_excess_deaths,"output-data/excess-deaths/switzerland_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export Turkey
-write.csv(get_excess_deaths(turkey_weekly_deaths),
-          "output-data/excess-deaths/turkey_excess_deaths.csv",
+turkey_excess_deaths <- get_excess_deaths(turkey_weekly_deaths)
+write.csv(turkey_excess_deaths,"output-data/excess-deaths/turkey_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
 
 # Export the United States
-write.csv(get_excess_deaths(united_states_weekly_deaths),
-          "output-data/excess-deaths/united_states_excess_deaths.csv",
+united_states_excess_deaths <- get_excess_deaths(united_states_weekly_deaths)
+write.csv(united_states_excess_deaths,"output-data/excess-deaths/united_states_excess_deaths.csv",
           fileEncoding = "UTF-8",row.names=FALSE)
+
+# Step 3: combine weekly and monthly deaths together, and calculate deaths per 100,000 people and percentage change ---------------------------------------
+
+# Combine weekly deaths and calculate per 100,000 people and percentage change
+all_weekly_excess_deaths <- bind_rows(austria_excess_deaths,
+                                      belgium_excess_deaths,
+                                      britain_excess_deaths,
+                                      chile_excess_deaths,
+                                      denmark_excess_deaths,
+                                      france_excess_deaths,
+                                      germany_excess_deaths,
+                                      italy_excess_deaths,
+                                      mexico_excess_deaths,
+                                      netherlands_excess_deaths,
+                                      norway_excess_deaths,
+                                      portugal_excess_deaths,
+                                      south_africa_excess_deaths,
+                                      spain_excess_deaths,
+                                      sweden_excess_deaths,
+                                      switzerland_excess_deaths,
+                                      turkey_excess_deaths,
+                                      united_states_excess_deaths) %>%
+  mutate(covid_deaths_per_100k = covid_deaths / population * 100000,
+         excess_deaths_per_100k = excess_deaths / population * 100000,
+         excess_deaths_pct_change = ((expected_deaths + excess_deaths) / expected_deaths) - 1)
+
+# Export weekly deaths
+write.csv(all_weekly_excess_deaths,"output-data/excess-deaths/all_weekly_excess_deaths.csv",
+          fileEncoding = "UTF-8",row.names=FALSE)
+
+# Combine monthly deaths and calculate per 100,000 people and percentage change
+all_monthly_excess_deaths <- bind_rows(brazil_excess_deaths,
+                                       ecuador_excess_deaths,
+                                       indonesia_excess_deaths,
+                                       peru_excess_deaths,
+                                       russia_excess_deaths) %>%
+  mutate(covid_deaths_per_100k = covid_deaths / population * 100000,
+         excess_deaths_per_100k = excess_deaths / population * 100000,
+         excess_deaths_pct_change = ((expected_deaths + excess_deaths) / expected_deaths) - 1)
+
+# Export monthly deaths
+write.csv(all_monthly_excess_deaths,"output-data/excess-deaths/all_monthly_excess_deaths.csv",
+          fileEncoding = "UTF-8",row.names=FALSE)
+  
